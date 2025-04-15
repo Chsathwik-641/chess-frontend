bc2cb355e15bd22ce3f7f3103feb02d03c02de9e
Chsathwik-641 <chepyalasathwik641@gmail.com>
2025-04-15 21:58:39 +0530

    final commit

--- src/Components/ChessBoard.jsx
+++ src/Components/ChessBoard.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Chessboard = ({ board, boardTheme, onMove, color, turn }) => {
   const [draggedPiece, setDraggedPiece] = useState(null);
   const [touchPiece, setTouchPiece] = useState(null);
+
+  const boardRef = useRef(null);
 
   const handleDragStart = (row, col) => {
     const piece = board[row][col];
@@ -16,22 +18,36 @@ const Chessboard = ({ board, boardTheme, onMove, color, turn }) => {
     setDraggedPiece(null);
   };
 
-  const handleTouchStart = (row, col) => {
+  const handleTouchStart = (e, row, col) => {
+    e.preventDefault();
+    const touch = e.touches[0];
+    const boardRefe = boardRef.current;
+    const rect = boardRefe.getBoundingClientRect();
+
+    const squareSize = rect.width / 8;
+    const x = touch.clientX - rect.left;
+    const y = touch.clientY - rect.top;
+
+    const colF = Math.floor(x / squareSize);
+    const rowF = Math.floor(y / squareSize);
     const piece = board[row][col];
     if (!piece || piece.color !== turn[0]) return;
-    setTouchPiece({ row, col });
+    setTouchPiece({ rowF, colF });
   };
 
-  const handleTouchEnd = (row, col) => {
+  const handleTouchEnd = (e, row, col) => {
     if (!touchPiece) return;
-    onMove({ row: touchPiece.row, col: touchPiece.col }, { row, col });
+    onMove({ row, col }, { row: touchPiece.rowF, col: touchPiece.colF });
     setTouchPiece(null);
   };
 
   if (!Array.isArray(board)) return <div>Invalid board</div>;
 
   return (
-    <div className={`chessboard ${color === "black" ? "black-view" : ""}`}>
+    <div
+      className={`chessboard ${color === "black" ? "black-view" : ""}`}
+      ref={boardRef}
+    >
       {board.map((rowArr, row) =>
         rowArr.map((piece, col) => {
           const isDark = (row + col) % 2 === 1;
@@ -46,13 +62,14 @@ const Chessboard = ({ board, boardTheme, onMove, color, turn }) => {
               className={squareClass}
               onDragOver={(e) => e.preventDefault()}
               onDrop={() => handleDrop(row, col)}
-              onTouchEnd={(e) => handleTouchEnd(row, col)}
+              onTouchEnd={(e) => handleTouchEnd(e, row, col)}
+              onTouchMove={(e) => handleTouchStart(e, row, col)}
             >
               {piece && (
                 <div
                   draggable
                   onDragStart={() => handleDragStart(row, col)}
-                  onTouchStart={(e) => handleTouchStart(row, col)}
+                  onTouchStart={(e) => handleTouchStart(e, row, col)}
                 >
                   <img src={`/Pieces/${pieceCode}.png`} alt={pieceCode} />
                 </div>
